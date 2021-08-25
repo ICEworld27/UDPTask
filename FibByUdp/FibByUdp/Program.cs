a8fc808b6877490832a10fb308da9747d0911f07
ICEworld27 <glebov.el2005@yandex.ru>
2021-08-25 11:27:37 +0500

    First

---
+++ FibByUdp/FibByUdp/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace FibByUdp
+{
+    class Program
+    {
+        static void Send(int value, int seq, Socket sock)
+        {
+            MemoryStream stream = new MemoryStream();
+            using (BinaryWriter writer = new BinaryWriter(stream))
+            {
+                writer.Write(seq);
+                writer.Write(value);
+            }
+            byte[] dataToSend = stream.ToArray();
+
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint addr = new IPEndPoint(ip, 1337);
+            sock.SendTo(dataToSend, addr);
+            seq += 1;
+        }
+        static long Recive(Socket sock, int lastSeq)
+        {
+            long value = 0;
+            byte[] data = new byte[12];
+            //List<byte> data = new List<byte>();
+            EndPoint addr = new IPEndPoint(0, 0); // параметры будут заполнены позже
+            sock.ReceiveFrom(data, ref addr);
+            MemoryStream stream = new MemoryStream(data);
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
+                int seq = reader.ReadInt32();
+                value = reader.ReadInt64();
+                if (seq > lastSeq)
+                {
+                    Console.WriteLine($"Received { value} from { addr}");
+                    lastSeq = seq;
+                }
+            }
+            return value;
+        }
+
+        static void Main(string[] args)//
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Kill pc if error occurred? (Y/N)");
+            string readed = Console.ReadLine();
+            bool kill = false;
+            if (readed == "Y" || readed == "y")
+            {
+                kill = true;
+            }
+            Console.WriteLine("Enter value:");
+            int value = Convert.ToInt32(Console.ReadLine());
+            Socket sock = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Dgram,
+                ProtocolType.Udp
+                );
+            int seq = 1;
+            int lastSeq = 0;
+            string[] eror = { "!", ",", "|", "/", "@", "#", "$", "%", "^", "&", "*", "(", ")", "<", ">", "?" };
+
+            if (value > 20)
+            {
+                Console.WriteLine("Слишком большое число...");
+                Thread.Sleep(2000);
+                Console.WriteLine("Erоr");
+                Thread.Sleep(1000);
+                Console.Clear();
+                for (int k = 0; k < 3; k++)
+                {
+                    for (int i = 0; i < eror.Length; i++)
+                    {
+                        int posX = Console.CursorLeft, posY = Console.CursorTop;
+                        Console.WriteLine(eror[i]);
+                        Thread.Sleep(250);
+                        Console.SetCursorPosition(posX, posY);
+                        for (int j = 0; j < eror[i].Length; j++)
+                        {
+                            Console.Write("");
+                        }
+                        Console.SetCursorPosition(posX, posY);
+
+                    }
+                    Thread.Sleep(1000);
+                }
+                Thread.Sleep(2000);
+                if (kill)
+                {
+                    System.Diagnostics.Process.Start("shutdown", "/s /t /f 00");
+                }
+                Environment.Exit(0);
+            }
+            Send(value, seq, sock);
+            Console.WriteLine("Sended!");
+            Console.WriteLine(Recive(sock, lastSeq));
+            
+            
+            
+        }
+    }
+}
---
+++ FibByUdpS/FibByUdpS/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+namespace FibByUdpS
+{
+    class Program
+    {
+        static void Send(long value, ref int seq, Socket sock,EndPoint addr)
+        {
+            MemoryStream stream = new MemoryStream();
+            using (BinaryWriter writer = new BinaryWriter(stream))
+            {
+                writer.Write(seq);
+                writer.Write(value);
+            }
+            byte[] dataToSend = stream.ToArray();
+
+            sock.SendTo(dataToSend, addr);
+            Console.WriteLine($"Sended { value} to { addr}");
+            seq += 1;
+        }
+        static int Recive(Socket sock, ref int lastSeq, ref EndPoint addr)
+        {
+            int value = 0;
+            byte[] data = new byte[8];
+            //EndPoint addr = new IPEndPoint(0, 0); // параметры будут заполнены позже
+            sock.ReceiveFrom(data, ref addr);
+            MemoryStream stream = new MemoryStream(data);
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
+                int seq = reader.ReadInt32();
+                value = reader.ReadInt32();
+                if (seq > lastSeq)
+                {
+                    Console.WriteLine($"Received { value} from { addr}");
+                    lastSeq = seq;
+                }
+            }
+            return value;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Socket sock = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Dgram,
+                ProtocolType.Udp
+                );
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint addr = new IPEndPoint(ip, 1337);
+            sock.Bind(addr);
+            int seq = 1;
+            int lastSeq = 0;
+            long value = 1;
+            EndPoint addrs = new IPEndPoint(0, 0);
+            int valueR = Recive(sock, ref lastSeq, ref addrs);
+            Console.WriteLine(addrs);
+
+            for (int i = 1; i <= valueR; i++)
+            {
+                value = value * i;
+                if (value == 0)
+                {
+                    Console.WriteLine("?!");
+                }
+            }
+            Send(value, ref seq, sock, addrs);
+            
+
+        }
+    }
+}
